9d859d6a1ba3849fcbe79aca8d2689631ac846fd
ksmithut <github@ksmithut.com>
2020-02-17 21:37:17 -0700

    initial commit

---
+++ functions/create-sandbox-users.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const { default: axios } = require('axios')
+
+const { KUALI_ADMIN_TOKEN, KUALI_HOST, EMAIL_KEY, PASSWORD } = process.env
+
+exports.handler = async (event, context) => {
+  const body = JSON.parse(event.body)
+  const email = body[EMAIL_KEY]
+  const createUserURL = `${KUALI_HOST}/api/v1/users`
+  const user = {
+    email,
+    password: PASSWORD
+  }
+  const headers = {
+    Authorization: `Bearer ${KUALI_ADMIN_TOKEN}`
+  }
+  const response = await axios.post(createUserURL, user, { headers })
+  return {
+    statusCode: 200,
+    body: 'ok'
+  }
+}
