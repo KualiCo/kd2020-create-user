50a1a8ea543504fb5bb351b6fe067843ab24c23c
ksmithut <github@ksmithut.com>
2020-02-18 06:37:31 -0700

    use auth token from integration

--- functions/create-sandbox-users.js
+++ functions/create-sandbox-users.js
@@ -2,9 +2,13 @@
 
 const { default: axios } = require('axios')
 
-const { KUALI_ADMIN_TOKEN, KUALI_HOST, EMAIL_KEY, PASSWORD } = process.env
+const { KUALI_HOST, EMAIL_KEY, PASSWORD } = process.env
 
 exports.handler = async (event, context) => {
+  const { authorization } = event.headers
+  if (!authorization) return { statusCode: 401 }
+  const token = authorization.replace(/^bearer\s+/i, '')
+  if (!token) return { statusCode: 401 }
   const body = JSON.parse(event.body)
   const email = body[EMAIL_KEY]
   const createUserURL = `${KUALI_HOST}/api/v1/users`
@@ -13,7 +17,7 @@ exports.handler = async (event, context) => {
     password: PASSWORD
   }
   const headers = {
-    Authorization: `Bearer ${KUALI_ADMIN_TOKEN}`
+    Authorization: `Bearer ${token}`
   }
   const response = await axios.post(createUserURL, user, { headers })
   return {
