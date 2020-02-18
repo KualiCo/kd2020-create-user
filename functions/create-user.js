481326a4486c046a73a478dc198fd21d8f007ac3
ksmithut <github@ksmithut.com>
2020-02-18 10:19:54 -0700

    simplify body

--- functions/create-user.js
+++ functions/create-user.js
@@ -5,17 +5,16 @@ const { default: axios } = require('axios')
 const { KUALI_HOST, EMAIL_KEY, NAME_KEY, PASSWORD } = process.env
 
 exports.handler = async (event, context) => {
+  // Get the authorization token
   const { authorization } = event.headers
   if (!authorization) return { statusCode: 401 }
   const token = authorization.replace(/^bearer\s+/i, '')
   if (!token) return { statusCode: 401 }
 
   const body = JSON.parse(event.body)
-  const email = body[EMAIL_KEY]
-  const name = body[NAME_KEY]
   const user = {
-    name,
-    email,
+    name: body[NAME_KEY],
+    email: body[EMAIL_KEY],
     password: PASSWORD
   }
   const createUserURL = `${KUALI_HOST}/api/v1/users`
